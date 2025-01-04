cb1cea03ce1368fc7e00a0c600954ab2d19830d8
NoirTech345 <103590217+NoirTech345@users.noreply.github.com>
2025-01-04 15:18:56 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -5,13 +5,18 @@ connection = sqlite3.connect('database.sqlite')
 cursor = sqlite3.Cursor(connection)
 
 cursor.execute('DROP TABLE IF EXISTS products')
-cursor.execute('DROP TABLE IF EEXISTS shopping_cart')
 
-request = ('CREATE TABLE IF EXISTS products'
+request = ('CREATE TABLE IF NOT EXISTS products'
            '(id INTEGRER PRIMARY KEY AUTOINCREMENT,'
            'name VARCHAR(255),'
            'price INTEGR,'
-           'depth INTEGER'
-           'image_name VARCHAR(255))'
-cursor.excute(request)
+           'category VARCHAR(255),'
+           'characteristics VARCHAR(255)'
+           'image_name VARCHAR(255))')
+cursor.execute(request)
+
+insert_request = ("INSERT INTO products"
+                 "(name, price, category, characteristics, image_name) VALUES (?, ?, ?, ?, ?)")
+
+cursor.execute(insert_request, ("Процесор Intel Core i9", 15000, "CPU", ""))
 
